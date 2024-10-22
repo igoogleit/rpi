3fc0663f3408a9bb25f480374efc630f5c8aba76
igoogleit <try.ishivam@gmail.com>
2024-10-22 09:13:20 +0530

    Update LCD.py

--- LCD.py
+++ LCD.py
@@ -23,7 +23,7 @@
 from time import sleep
 from RPLCD.i2c import charLCD
 
-lcd=charLCD(i2c_expander='PCF8574',address=0x27,port=1,cols=16,rows=2,datsize=8)
+lcd=charLCD(i2c_expander='PCF8574',address=0x27,port=1,cols=16,rows=2,dotsize=8)
 
 lcd.clear()
 lcd.write_string(str("Hello Guys"))
